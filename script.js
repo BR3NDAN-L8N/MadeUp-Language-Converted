02936f4e36e175381dc7dd318b68ee1d37a5ecfe
Game Maker <br3ndan.l8n@gmail.com>
2020-07-10 19:57:06 -0400

    Special Characters and Numbers will now be taken off the end of a word, or if by them selves, no longer hav an ending added to them.

--- script.js
+++ script.js
@@ -1,33 +1,61 @@
 const englishText = document.getElementById('englishText');
 const azgarishText = document.querySelector('#azgarishText');
 const translateBTN = document.querySelector('#translate');
+
+
 let newString = "";
-vowels = /a|e|i|o|u|y/i;
-vowelAtEnd = /a|e|i|o|u|y\b/i;
-word = "";
-oldWord = "";
-newWord = "";
+const vowels = /a|e|i|o|u|y/i;
+const alphanumeric = /[a-z]/i;
+const notALetter = /[^a-z]/i;
+let word = "";
+let oldWord = "";
+let newWord = "";
 
 translateBTN.addEventListener('click', translate);
 
 function translate() {
-    // console.log(englishText.value);
+    console.log(englishText.value);
+    let translation = "";
+    phraseToTranslate = englishText.value;
+    const words = englishText.value.split(" ");
 
-
+    for (i = 0; i < words.length; i++) {
+        newString += `${addEnding(words[i])} `;
+    }
+    console.log(newString);
+    azgarishText.textContent = newString;
 }
 
 // Add an ending based on the last letter being a vowel or not
-function addEnding(oldWord, newWord) {
-    if (vowels.test(oldWord.charAt(oldWord.length-1))) {
-        newWord += "gar";
-    } else {
-        newWord += "ah";
+function addEnding(wordToCheck) {
+    // This variable will hold all the characters at the end of a word that are not letters
+    notLetters = "";
+    // Checking if the words has any non-letter characters at the end
+    if (notALetter.test(wordToCheck.charAt(wordToCheck.length-1))) {
+        // i starts as 1 as we deduct i from the word.length to access the last character
+        // the conditional in the middle returns True if the character is not a letter
+        for (i = 1; notALetter.test(wordToCheck.charAt(wordToCheck.length-i)) ; i++) {
+            let tempCharHolder = wordToCheck.charAt(wordToCheck.length-i);
+            // we concatinate like this to keep multiple non-letter characters in their original order
+            notLetters = tempCharHolder + notLetters;
+        }
     }
-    console.log(newWord);
-    return newWord;   
+    // removing the non-letters from the word we are checking
+    wordToCheck = wordToCheck.replace(notLetters, "");
+    console.log(`this is ${notALetter.test(wordToCheck.charAt(wordToCheck.length-1))}`);
+
+    // adding the appropriate ending to the word and then adding back the non-letters we removed previously... vowel endings get the truthy, non-vowels get the falsey
+    if (vowels.test(wordToCheck.charAt(wordToCheck.length-1))) {
+        wordToCheck += "gar" + notLetters;
+    } else if (alphanumeric.test(wordToCheck.charAt(wordToCheck.length-1))) {
+        wordToCheck += "ah" + notLetters;
+    } else {
+        wordToCheck = notLetters;
+    }
+    return wordToCheck;   
 }
 
 
 // const lastLetter = oldWord.charAt(oldWord.length-1);
 // console.log(/a|e|i|o|u|y/i.test(lastLetter));
-addEnding(oldWord, newString);
+// addEnding(oldWord, newString);
