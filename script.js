2974cb9132c016e50b5f092fb74f54bacc064f2b
Game Maker <br3ndan.l8n@gmail.com>
2020-07-10 16:45:53 -0400

    Finished function for adding the ending to the new word.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const englishText = document.getElementById('englishText');
 const azgarishText = document.querySelector('#azgarishText');
 const translateBTN = document.querySelector('#translate');
+let newString = "";
+vowels = /a|e|i|o|u|y/i;
+vowelAtEnd = /a|e|i|o|u|y\b/i;
+word = "";
+oldWord = "";
+newWord = "";
 
 translateBTN.addEventListener('click', translate);
 
@@ -10,10 +16,18 @@ function translate() {
 
 }
 
-function addEnding(letter) {
-    if (letter /aeiouy/i) {
-        newString += "gar";
+// Add an ending based on the last letter being a vowel or not
+function addEnding(oldWord, newWord) {
+    if (vowels.test(oldWord.charAt(oldWord.length-1))) {
+        newWord += "gar";
     } else {
-        newString += "ah";
+        newWord += "ah";
     }
+    console.log(newWord);
+    return newWord;   
 }
+
+
+// const lastLetter = oldWord.charAt(oldWord.length-1);
+// console.log(/a|e|i|o|u|y/i.test(lastLetter));
+addEnding(oldWord, newString);
